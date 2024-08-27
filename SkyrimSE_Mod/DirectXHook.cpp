71cae4abd33c246766b9e8296713a783df510ec3
CyN1ckal <warrior276276276@gmail.com>
2024-08-27 13:15:43 -0700

    DirectXHook re-factor

--- SkyrimSE_Mod/DirectXHook.cpp
+++ SkyrimSE_Mod/DirectXHook.cpp
@@ -1,43 +1,83 @@
 #include "pch.h"
 
-extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg,
-                                              WPARAM wParam, LPARAM lParam);
-LRESULT __stdcall DirectXHook::SKSE_WndProc(const HWND hWnd, UINT uMsg,
-                                            WPARAM wParam, LPARAM lParam) {
+bool DirectXHook::Initialize() {
+  SkyrimSE_ProcessId = GetCurrentProcessId();
+
+  SkyrimSE_hWnd = FindWindowA(NULL, "Skyrim Special Edition");
+
+  VersionCheck->Initialize();
+
+  if (VersionCheck->CommunityShadersFound) {
+    Console::PrintSuccess("CommunityShaders.dll Found!");
+
+    DirectXHook::GetCommunityShadersPresentPointer();
+
+  } else {
+    Console::PrintSuccess("Non-Modded Version Found!");
+
+    DirectXHook::GetPresentPointer();
+  }
+
+  if (!PresentFunctionAddress) {
+    Console::PrintError("PresentFunctionAddress");
+    printf("%X\n", GetLastError());
+    return 0;
+  }
+
+  if (!DirectXHook::EnableDirectXHooks()) {
+    Console::PrintError("EnableDirectXHooks Failed!");
+    return 0;
+  }
+
+  return 1;
+}
+
+bool DirectXHook::Uninitialize() {
+  SetWindowLongPtr(DirectXHook::SkyrimSE_hWnd, GWLP_WNDPROC,
+                   (LONG_PTR)DirectXHook::SKSE_Original_WndProc);
+  return 1;
+}
+
+bool DirectXHook::EnableDirectXHooks() {
+
+  if (MH_CreateHook((void *)PresentFunctionAddress, &Present_Hooked,
+                    reinterpret_cast<LPVOID *>(&Present_Original)) != MH_OK) {
+    printf("PresentFunctionAddress MH_CreateHook Failed!\n");
+    return 0;
+  }
+
+  if (MH_EnableHook((void *)PresentFunctionAddress) != MH_OK) {
+    printf("PresentFunctionAddress MH_EnableHook Failed!\n");
+    return 0;
+  }
+
+  return 1;
+}
+
+long __stdcall DirectXHook::Present_Hooked(IDXGISwapChain *SwapChain,
+                                           UINT SyncInterval, UINT Flags) {
+  if (!D3D11_Device && !DirectXHook::GetD3D11Device(SwapChain)) {
+    Console::PrintError("GetD3D11Device Error!");
+    return Present_Original(SwapChain, SyncInterval, Flags);
+  }
+
+  if (MyImGui::m_ImGuiInitialized) {
+    MyImGui::OnFrame();
+    ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+  }
+
+  return Present_Original(SwapChain, SyncInterval, Flags);
+}
+
+LRESULT __stdcall DirectXHook::SKSE_MyWndProc(const HWND hWnd, UINT uMsg,
+                                              WPARAM wParam, LPARAM lParam) {
   // Reminder, I need to set the MouseHandleToggle if I want messages to come
   // here...
 
   if (true && ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam))
     return true;
 
-  return CallWindowProc(SKSE_WndProc_Original, hWnd, uMsg, wParam, lParam);
-}
-
-BOOL DirectXHook::EnumWindowsProc(HWND hWnd, LPARAM lParam) {
-
-  DWORD CurrentHWNDProcID;
-  GetWindowThreadProcessId(hWnd, &CurrentHWNDProcID);
-
-  if (CurrentHWNDProcID != SkyrimSE_ProcessId)
-    return 1;
-
-  std::wstring title(GetWindowTextLength(hWnd) + 1, L'\0');
-  GetWindowTextW(hWnd, &title[0], title.size());
-
-  std::wstring SKSE_Title = L"Skyrim Special Edition";
-
-  int ReturnValue = wcscmp(title.c_str(), SKSE_Title.c_str());
-
-  if (!ReturnValue) {
-    SkyrimSE_hWnd = hWnd;
-
-    SKSE_WndProc_Original = (WNDPROC)SetWindowLongPtr(
-        SkyrimSE_hWnd, GWLP_WNDPROC, (LONG_PTR)SKSE_WndProc);
-
-    return 1;
-  }
-
-  return 1;
+  return CallWindowProc(SKSE_Original_WndProc, hWnd, uMsg, wParam, lParam);
 }
 
 bool DirectXHook::GetPresentPointer() {
@@ -73,101 +113,49 @@ bool DirectXHook::GetPresentPointer() {
   Device->Release();
 
   PresentFunctionAddress = (uintptr_t)p_vtable[8];
-  //printf("Present Function %llx\n", PresentFunctionAddress);
+  // printf("Present Function %llx\n", PresentFunctionAddress);
 
   return 1;
 }
 
-bool DirectXHook::Initialize() {
-  SkyrimSE_ProcessId = GetCurrentProcessId();
+bool DirectXHook::GetD3D11Device(IDXGISwapChain *SwapChain) {
 
-  EnumWindows(DirectXHook::EnumWindowsProc, NULL);
+  if (SUCCEEDED(SwapChain->GetDevice(__uuidof(ID3D11Device),
+                                     (void **)&D3D11_Device))) {
 
-  VersionCheck->Initialize();
+    D3D11_Device->GetImmediateContext(&D3D11_DeviceContext);
 
-  if (VersionCheck->CommunityShadersFound) {
-    Console::PrintSuccess("CommunityShaders.dll Found!");
+    ID3D11Texture2D *pBackBuffer;
 
-    DirectXHook::PresentFunctionAddress =
-        PatternScan::Internal::PatternScanModule_ComboPattern(
-            "CommunityShaders.dll",
-            Signatures::CommunityShadersPresentFunctionSignature,
-            Signatures::CommunityShadersPresentFunctionSignatureLength);
-  } else {
-    Console::PrintSuccess("Non-Modded Version Found!");
+    SwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID *)&pBackBuffer);
 
-    if (!DirectXHook::GetPresentPointer()) {
-      Console::PrintError("GetPresentPointer Failed!");
-      return 0;
-    }
+    D3D11_Device->CreateRenderTargetView(pBackBuffer, NULL,
+                                         &MainRenderTargetView);
+
+    pBackBuffer->Release();
+
+    MyImGui::Initialize(D3D11_Device, D3D11_DeviceContext, SkyrimSE_hWnd);
+
+    return 1;
   }
+
+  Console::PrintError("SwapChain->GetDevice Failed!");
+
+  return 0;
+}
+
+bool DirectXHook::GetCommunityShadersPresentPointer() {
+
+  PresentFunctionAddress =
+      PatternScan::Internal::PatternScanModule_ComboPattern(
+          "CommunityShaders.dll",
+          Signatures::CommunityShadersPresentFunctionSignature,
+          Signatures::CommunityShadersPresentFunctionSignatureLength);
 
   if (!PresentFunctionAddress) {
-    Console::PrintError("PresentFunctionAddress");
-    printf("%X\n", GetLastError());
+    Console::PrintError("GetCommunityShadersPresentPointer Failed!");
     return 0;
   }
 
-  if (!DirectXHook::EnableDirectXHooks()) {
-    Console::PrintError("EnableDirectXHooks Failed!");
-    return 0;
-  }
-
-  return 1;
-}
-
-Present_Template Present_Original = nullptr;
-long __stdcall DirectXHook::Present_Hooked(IDXGISwapChain *SwapChain,
-                                           UINT SyncInterval, UINT Flags) {
-  if (!m_DXInitialized) {
-    if (SUCCEEDED(SwapChain->GetDevice(__uuidof(ID3D11Device),
-                                       (void **)&D3D11_Device))) {
-      D3D11_Device->GetImmediateContext(&D3D11_DeviceContext);
-      ID3D11Texture2D *pBackBuffer;
-      SwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D),
-                           (LPVOID *)&pBackBuffer);
-      D3D11_Device->CreateRenderTargetView(pBackBuffer, NULL,
-                                           &MainRenderTargetView);
-      pBackBuffer->Release();
-
-      m_DXInitialized = true;
-
-      MyImGui::Initialize(D3D11_Device, D3D11_DeviceContext, SkyrimSE_hWnd);
-
-      // ESP::Initialize();
-
-    } else {
-      printf("SwapChain->GetDevice Error!\n");
-      return Present_Original(SwapChain, SyncInterval, Flags);
-    }
-  }
-
-  if (MyImGui::m_ImGuiInitialized) {
-    MyImGui::OnFrame();
-    ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
-  }
-
-  return Present_Original(SwapChain, SyncInterval, Flags);
-}
-
-bool DirectXHook::EnableDirectXHooks() {
-
-  if (MH_CreateHook((void *)PresentFunctionAddress, &Present_Hooked,
-                    reinterpret_cast<LPVOID *>(&Present_Original)) != MH_OK) {
-    printf("PresentFunctionAddress MH_CreateHook Failed!\n");
-    return 0;
-  }
-
-  if (MH_EnableHook((void *)PresentFunctionAddress) != MH_OK) {
-    printf("PresentFunctionAddress MH_EnableHook Failed!\n");
-    return 0;
-  }
-
-  return 1;
-}
-
-bool DirectXHook::Uninitialize() {
-  SetWindowLongPtr(DirectXHook::SkyrimSE_hWnd, GWLP_WNDPROC,
-                   (LONG_PTR)DirectXHook::SKSE_WndProc_Original);
   return 1;
 }
--- SkyrimSE_Mod/DirectXHook.h
+++ SkyrimSE_Mod/DirectXHook.h
@@ -2,44 +2,44 @@
 
 #include "VersionCheck.h"
 
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg,
+                                              WPARAM wParam, LPARAM lParam);
+
 typedef long(__stdcall *Present_Template)(IDXGISwapChain *, UINT, UINT);
 
 class DirectXHook {
 public:
-  static inline ID3D11Device *D3D11_Device;
-  static inline ID3D11DeviceContext *D3D11_DeviceContext;
-  static inline ID3D11RenderTargetView *MainRenderTargetView;
-
-  static inline HWND SkyrimSE_hWnd;
-  static inline HWND FullPath_hWnd;
-  static inline HWND SwapChainOutputhWnd;
-  static inline DWORD SkyrimSE_ProcessId;
-
-  static inline uintptr_t PresentFunctionAddress;
-
+  // Interface functions
   static bool Initialize();
-
-  static bool GetPresentPointer();
-
+  static bool Uninitialize();
   static bool EnableDirectXHooks();
 
+public:
+  // "Globals"
+  static inline ID3D11Device *D3D11_Device = 0;
+  static inline ID3D11DeviceContext *D3D11_DeviceContext = 0;
+  static inline ID3D11RenderTargetView *MainRenderTargetView = 0;
+  static inline HWND SkyrimSE_hWnd = 0;
+  static inline DWORD SkyrimSE_ProcessId = 0;
+  static inline uintptr_t PresentFunctionAddress = 0;
+
+public:
+  // Helper Classes
+  static VersionCheckClass *VersionCheck;
+
+private:
+  // Private "hook" functions
+  static inline Present_Template Present_Original = nullptr;
   static long __stdcall Present_Hooked(IDXGISwapChain *SwapChain,
                                        UINT SyncInterval, UINT Flags);
 
-  static BOOL CALLBACK EnumWindowsProc(HWND hWnd, LPARAM lParam);
+  static inline WNDPROC SKSE_Original_WndProc;
+  static LRESULT __stdcall SKSE_MyWndProc(const HWND hWnd, UINT uMsg,
+                                          WPARAM wParam, LPARAM lParam);
 
-  static inline bool m_DXInitialized = false;
-
-  static LRESULT __stdcall SKSE_WndProc(const HWND hWnd, UINT uMsg,
-                                        WPARAM wParam, LPARAM lParam);
-
-  static LRESULT __stdcall FullPath_WndProc(const HWND hWnd, UINT uMsg,
-                                            WPARAM wParam, LPARAM lParam);
-
-  static inline WNDPROC SKSE_WndProc_Original;
-
-  static bool Uninitialize();
-
-public:
-  static VersionCheckClass *VersionCheck;
+private:
+  // Private functions
+  static bool GetPresentPointer();
+  static bool GetCommunityShadersPresentPointer();
+  static bool GetD3D11Device(IDXGISwapChain *SwapChain);
 };
